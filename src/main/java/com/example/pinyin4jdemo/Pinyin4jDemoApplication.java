62f13c393e7a64b262bf993822bd5f2d08efb0d2
huangwc <huangwencon@foxmail.com>
2020-08-25 18:49:24 +0800

    Initial commit

---
+++ src/main/java/com/example/pinyin4jdemo/Pinyin4jDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinyin4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pinyin4jDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pinyin4jDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pinyin4jdemo/Pinyin4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pinyin4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pinyin4jDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
